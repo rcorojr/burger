c1fa52571a74ed1837d4f2a8669647524514458d
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-07-16 11:47:17 -0600

    add seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey burger', false);
